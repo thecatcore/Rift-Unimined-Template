7d5014ddd46a155a42df32e47cfaee020ce6536f
Cat Core <arthur.koalas@free.fr>
2023-12-20 19:07:46 +0100

    Working Rift-MDK

--- buildSrc/src/main/kotlin/xyz/wagyourtail/unimined/minecraft/patch/rift/RiftPatch.kt
+++ buildSrc/src/main/kotlin/xyz/wagyourtail/unimined/minecraft/patch/rift/RiftPatch.kt
@@ -4,6 +4,7 @@ import org.gradle.api.Project
 import org.gradle.api.artifacts.ModuleDependency
 import xyz.wagyourtail.unimined.api.minecraft.patch.ataw.AccessTransformerPatcher
 import xyz.wagyourtail.unimined.api.runs.RunConfig
+import xyz.wagyourtail.unimined.api.unimined
 import xyz.wagyourtail.unimined.internal.minecraft.MinecraftProvider
 import xyz.wagyourtail.unimined.internal.minecraft.patch.AbstractMinecraftTransformer
 import xyz.wagyourtail.unimined.internal.minecraft.patch.access.transformer.AccessTransformerMinecraftTransformer
@@ -13,21 +14,39 @@ import xyz.wagyourtail.unimined.util.LazyMutable
 open class RiftPatch(
     project: Project,
     provider: MinecraftProvider,
-) : AbstractMinecraftTransformer(project, provider, "Rift") {
     private val accessTransformerMinecraftTransformer: AccessTransformerMinecraftTransformer = AccessTransformerMinecraftTransformer(project, provider, "Rift")
+) : AbstractMinecraftTransformer(project, provider, "Rift"), AccessTransformerPatcher by accessTransformerMinecraftTransformer {
+
+    companion object {
+        @JvmStatic
+        open fun riftPatch(
+            project: Project,
+            provider: MinecraftProvider
+        ): RiftPatch {
+            return RiftPatch(project, provider)
+        }
+    }
 
     open var loader by FinalizeOnRead(LazyMutable {
-        project.files("libs/Rift-FINAL.jar")
-    });
+        "FINAL"
+    })
+
+    open fun loader(version: String) {
+        loader = version;
+    }
+
+    init {
+        project.unimined.minecraftForgeMaven()
+    }
 
     override fun apply() {
         createRiftDependency("org.spongepowered:mixin:0.7.11-SNAPSHOT")
         createRiftDependency("net.minecraft:launchwrapper:1.12")
         createRiftDependency("org.ow2.asm:asm:6.2", true)
         createRiftDependency("org.ow2.asm:asm-commons:6.2", true)
+        createRiftDependency("org.ow2.asm:asm-tree:6.2", true)
 
-        val rift = project.dependencies.create(loader)
-        provider.minecraftLibraries.dependencies.add(rift)
+        project.dependencies.add("modImplementation", project.dependencies.create("org.dimdev:Rift:${loader}"))
 
         super.apply()
     }
